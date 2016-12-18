490ad2b6c9d76718885d7092d4dd5d4ae8b24012
Martin Fenner <martin.fenner@datacite.org>
2016-12-18 20:58:57 +0100

    add aruba gem

--- Rakefile
+++ Rakefile
@@ -1,5 +1,31 @@
 #!/usr/bin/env rake
 
-# Get useful helper tasks from Bundler
-require "bundler/gem_tasks"
+require 'bundler'
+require 'rake'
+require 'yaml'
+require 'rspec/core/rake_task'
 
+Bundler::GemHelper.install_tasks
+RSpec::Core::RakeTask.new('spec')
+
+namespace :repo do
+  desc "push files generated with cirneco to remote"
+  task :push do
+
+    # Configure git if this is run in Travis CI
+    if ENV["TRAVIS"]
+      sh "git config --global user.name '#{ENV['GIT_NAME']}'"
+      sh "git config --global user.email '#{ENV['GIT_EMAIL']}'"
+      sh "git config --global push.default simple"
+    end
+
+    # Commit and push to github
+    sh "git add --all ."
+    sh "git commit -m 'Committing changed files.'"
+    sh "git push https://${GH_TOKEN}@github.com/datacite/cirneco.rb master --quiet"
+    puts "Pushed changed files to repo"
+  end
+end
+
+# default task is running rspec tests
+task :default => :spec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,63 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.before do
+    ARGV.replace []
+  end
 end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__) + '/'
+end
+
+# This code was adapted from Thor, available under MIT-LICENSE
+# Copyright (c) 2008 Yehuda Katz, Eric Hodel, et al.
+def capture(stream)
+  begin
+    stream = stream.to_s
+    eval "$#{stream} = StringIO.new"
+    yield
+    result = eval("$#{stream}").string
+  ensure
+    eval("$#{stream} = #{stream.upcase}")
+  end
+
+  result
+end
+
+def capture_stdout(&block)
+  original_stdout = $stdout
+  $stdout = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stdout = original_stdout
+  end
+  fake.string
+end
+
+def capture_stderr(&block)
+  original_stderr = $stderr
+  $stderr = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stderr = original_stderr
+  end
+  fake.string
+end
+
+# This code was adapted from Ruby on Rails, available under MIT-LICENSE
+# Copyright (c) 2004-2013 David Heinemeier Hansson
+def silence_warnings
+  old_verbose, $VERBOSE = $VERBOSE, nil
+  yield
+ensure
+  $VERBOSE = old_verbose
+end
+
+alias silence capture
 
 VCR.configure do |c|
   mds_token = Base64.encode64("#{ENV['MDS_USERNAME']}:#{ENV['MDS_PASSWORD'].to_s}").rstrip
@@ -32,8 +88,4 @@ VCR.configure do |c|
   c.ignore_hosts 'codeclimate.com'
   c.filter_sensitive_data("<MDS_TOKEN>") { mds_token }
   c.configure_rspec_metadata!
-end
-
-def fixture_path
-  File.expand_path("../fixtures", __FILE__) + '/'
 end
