ef7b34e5022a41d79b1e9b1895415477a017b6c2
Kristian Garza <kj.garza@gmail.com>
2019-01-08 11:49:54 +0100

    update spec

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -97,7 +97,8 @@ describe Cirneco::Work, vcr: true, :order => :defined do
 
     describe "get_rest_doi" do
       it 'should get doi' do
-        response = subject.get_rest_doi(subject.doi, options)
+        r = subject.get_rest_doi(subject.doi, options)
+        response = JSON.parse(r.body.fetch("data", []))
         expect(response.dig("data","id")).to eq("10.5438/6brg-2m37")
       end
     end
